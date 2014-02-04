3870fbd323644508326252741cce979ba2f9a87a
Craig Sakuma <craig.sakuma@gmail.com>
2014-02-03 21:05:20 -0800

    added empty/initial files for hw #3

---
+++ hw3/hw3_data_scrape.py
@@ -0,0 +1,52 @@
+#get data
+#histograms
+#build rf model
+#evaluate rf model using roc curve and plot
+#include t-test
+import urllib
+#urllib2 might be better library
+import os
+import json
+import time
+import random
+import matplotlib
+#import requests #alternate to urllib
+dir_out = '/home/rob/data/yahoo_data_20140116'
+
+if not os.path.exists(dir_out):
+    print 'Creating directory %s' % dir_out
+    os.mkdir(dir_out)
+
+#symbols = ['FB','GOOG','AAPL']
+symbols = ['FB']
+#symbols  = json.loads(open("symbols.json").read())
+for symbol in symbols:
+    time.sleep(.5+random.random())
+    data_out = {}
+    if 'more than 25' not in data_out:
+        data_out['more than 25']=0
+    if 'less than 25' not in data_out:
+            data_out['less than 25']=0
+    try:
+        #url = 'http://ichart.finance.yahoo.com/table.csv?s=%s&d=6&e=23&f=2013&g=d&a=8&b=7&c=1984&ignore=.csv' % symbol
+        url = 'http://ichart.finance.yahoo.com/table.csv?s=%s&d=0&e=15&f=2014&g=d&a=4&b=18&c=2012&ignore=.csv' % symbol
+        print url
+        data = urllib.urlopen(url).read()
+        file = '%s/%s_data.csv' % (dir_out,symbol)
+        f = open(file,'w')
+        f.write('%s' % str(data))
+        f.close()
+        print 'wrote file %s' % file
+        data = data.split('\n')
+        for d in data:
+            d = d.split(',')
+            try:
+                close = d[6]
+                if float(close) >= float(25):
+                    data_out['more than 25']+=1
+                elif float(close) < float(25):
+                    data_out['less than 25']+=1
+            except:pass
+        print symbol,':', json.dumps(data_out)
+    except:
+        print 'failed for %s' % symbol
