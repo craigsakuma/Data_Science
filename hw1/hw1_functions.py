dff2d7aeabc331617ad27945306d4a1313e8adc2
Craig Sakuma <craig.sakuma@gmail.com>
2014-02-10 23:56:34 -0800

    updated documentation and filenames for hw1

--- hw1/hw1_functions.py
+++ hw1/hw1_functions.py
@@ -1,3 +1,5 @@
+#This contains functions that will be called by a wrapper - test.py
+
 from sklearn import datasets
 from sklearn.neighbors  import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -6,34 +8,28 @@ from sklearn.cross_validation import KFold
 
 def load_iris_data():
     # loads iris dataset 
-
     iris = datasets.load_iris()
 
     return (iris.data, iris.target, iris.target_names)
 
 
 def knn(X_train, y_train, k_neighbors = 3):
-    # method returns a kNN object called clf with methods:
-    #     score(X_test, y_test) --> to score the model using a test
-    #     predict(X_classify, y_test) --> to predict a result using
-
+    # method returns a kNN object called clf with methods score and predict
     clf = KNeighborsClassifier(k_neighbors)
     clf.fit(X_train, y_train)
 
     return clf
 
 def naive_bayes(X_train, y_train):
-    # method returns a NB object called clf with methods
-
-    gnb = GaussianNB()
-    clf = gnb.fit(X_train, y_train)
+    # method returns a NB object called clf with methods score and predict
+    clf = GaussianNB()
+    clf.fit(X_train, y_train)
 
     return clf
 
 
 def cross_validate(XX, yy, classifier, k_fold):
     # function returns generic cross validation
-
     # derive a set of random training and testing indices
     k_fold_indices = KFold(len(XX), n_folds=k_fold, indices=True, shuffle=True, random_state=0)
 
@@ -43,15 +39,6 @@ def cross_validate(XX, yy, classifier, k_fold):
 
         model = classifier(XX[[ train_slice ]],
                            yy[[ train_slice ]])
-
-
-#        if classifier == "KNN":
-#            model = knn(XX[[ train_slice ]],
-#                        yy[[ train_slice ]])
-
-#        elif classifier == "NB":
-#            model = naive_bayes(XX[[ train_slice ]],
-#                                yy[[ train_slice ]]) 
         
         k_score = model.score(XX[[ test_slice ]],
                               yy[[ test_slice ]])
--- hw1/hw1_wrapper.py
+++ hw1/hw1_wrapper.py
@@ -1,25 +1,35 @@
+# Wrapper that imports iris data set and scores a classifier (KNN and Naive Bayes) using a variety
+# of cross validation scenarios to determine optimal sample size for training the classifier which avoids over fitting 
 
-from hw1 import load_iris_data, cross_validate, knn, naive_bayes
+from hw1_functions import load_iris_data, cross_validate, knn, naive_bayes
 import argparse
 
-# argparse that allows users to select  either KNN or NaiveBayes algorithms only
+# argparse that allows users to select only KNN classifier or only NaiveBayes algorithms.  Default will run both classifiers
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-k","--KNN_only",action="store_true")
 group.add_argument("-nb","--NaiveBayes_only",action="store_true")
 args = parser.parse_args()
 
+# load iris data set
 (XX, yy, y) = load_iris_data()
 
+# if argparse is used to select KNN_only
 if args.KNN_only:
     classifiers_to_cv = [("kNN",knn)]
 
+# if argparse is used to select NaiveBayes_only
 elif args.NaiveBayes_only:
     classifiers_to_cv = [("Naive Bayes",naive_bayes)]
 
+# default selection which includes both classifiers if argparse isn't used
 else:
     classifiers_to_cv = [("kNN",knn),("Naive Bayes",naive_bayes)]
 
+# for each classifier
+# train and score the model using n folds cross validation
+# for each value of n calculate and print the average of all the scores from the cross validation
+# print the highest accuracy n fold with score  
 for (c_label, classifier) in classifiers_to_cv:
 
     print "\n---> %s <---" % c_label
